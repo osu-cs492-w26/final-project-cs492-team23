e9f7a9a54fcef3b10fe6b27253b378e720e9c4c3
Tea Kidder <teakidder@gmail.com>
2026-03-16 00:08:14 -0700

    Added Header Bar with Titles

--- app/src/main/java/edu/oregonstate/cs492/MovieWatchListManager/ui/MainActivity.kt
+++ app/src/main/java/edu/oregonstate/cs492/MovieWatchListManager/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package edu.oregonstate.cs492.MovieWatchListManager.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.oregonstate.cs492.MovieWatchListManager.R
 
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home, R.id.search, R.id.saved, R.id.history)
+        )
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.top_app_bar)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNav.setupWithNavController(navController)
     }
