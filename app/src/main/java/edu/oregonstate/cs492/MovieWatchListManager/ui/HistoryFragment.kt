b25df00053985dc8b54c90ab509d42c15eba7f54
Tea Kidder <teakidder@gmail.com>
2026-03-16 04:19:39 -0700

    Re-used RecyclerView for saved and watched movie pages w/random movie placeholders

--- app/src/main/java/edu/oregonstate/cs492/MovieWatchListManager/ui/HistoryFragment.kt
+++ app/src/main/java/edu/oregonstate/cs492/MovieWatchListManager/ui/HistoryFragment.kt
@@ -3,10 +3,31 @@ package edu.oregonstate.cs492.MovieWatchListManager.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.oregonstate.cs492.MovieWatchListManager.R
+import kotlinx.coroutines.launch
 
 class HistoryFragment : Fragment(R.layout.fragment_history) {
+    private val viewModel: TopRatedMoviesViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val rvHistoryMovies = view.findViewById<RecyclerView>(R.id.rv_history_movies)
+        rvHistoryMovies.layoutManager = LinearLayoutManager(requireContext())
+
+        viewModel.searchResults.observe(viewLifecycleOwner) { movies ->
+            movies?.let {
+                //  "random" watched movies
+                rvHistoryMovies.adapter = CategoryMovieAdapter(it.shuffled().take(8))
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.loadTopRatedMovies()
+        }
     }
 }
--- app/src/main/java/edu/oregonstate/cs492/MovieWatchListManager/ui/SavedFragment.kt
+++ app/src/main/java/edu/oregonstate/cs492/MovieWatchListManager/ui/SavedFragment.kt
@@ -3,10 +3,31 @@ package edu.oregonstate.cs492.MovieWatchListManager.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.oregonstate.cs492.MovieWatchListManager.R
+import kotlinx.coroutines.launch
 
 class SavedFragment : Fragment(R.layout.fragment_saved) {
+    private val viewModel: PopularMoviesViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val rvSavedMovies = view.findViewById<RecyclerView>(R.id.rv_saved_movies)
+        rvSavedMovies.layoutManager = LinearLayoutManager(requireContext())
+
+        viewModel.searchResults.observe(viewLifecycleOwner) { movies ->
+            movies?.let {
+                //"random" saved movies
+                rvSavedMovies.adapter = CategoryMovieAdapter(it.shuffled().take(6))
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.loadPopularMovies()
+        }
     }
 }
